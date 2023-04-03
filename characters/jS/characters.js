4df4bf4b1f58ea3c2a9ec2fe15150c9044d3f4f7
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2023-04-03 20:56:16 +0300

    .

---
+++ characters/jS/characters.js
@@ -0,0 +1,17 @@
+console.log('hi');
+
+let players = {
+		playerName : "KDB",
+		stats : {
+			offensiveAwareness : 84,
+			ballControl : 92,
+			lowPass : 94,
+			loftedPass : 94,
+			finishing : 78,
+			placeKicking : 80,
+			curl : 87,
+			speed : 78,
+			kickingPower : 90
+		}
+}
+console.log(players);
