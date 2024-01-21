2a4d2461bf686997a66f24fd823372048d360f0b
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2024-01-21 17:00:46 +0300

    menu popup complete

--- UI/jS/script.js
+++ UI/jS/script.js
@@ -1,1 +1,14 @@
 console.log('hi')
+
+const toggleMenu = document.querySelector('.popup-menu div i')
+const popupMenu = document.querySelector('.popup-menu .menu')
+
+toggleMenu.addEventListener('click', () => {
+    if (toggleMenu.classList.contains("bx-x")) {
+        toggleMenu.classList.replace("bx-x", "bx-slider")
+        popupMenu.style.display = 'none'
+    }else {
+        toggleMenu.classList.replace("bx-slider", "bx-x")
+        popupMenu.style.display = ''
+    }   
+})
