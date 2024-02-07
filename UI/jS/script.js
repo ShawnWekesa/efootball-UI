609ff8e2b9149945a081e2759387fe4e09a27c08
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2024-02-07 11:33:29 +0300

    toggleMenu

--- UI/jS/script.js
+++ UI/jS/script.js
@@ -4,11 +4,11 @@ const toggleMenu = document.querySelector('.popup-menu div i')
 const popupMenu = document.querySelector('.popup-menu .menu')
 
 toggleMenu.addEventListener('click', () => {
-    if (toggleMenu.classList.contains("bx-x")) {
-        toggleMenu.classList.replace("bx-x", "bx-slider")
-        popupMenu.style.display = 'none'
-    }else {
+    if (toggleMenu.classList.contains("bx-slider")) {
         toggleMenu.classList.replace("bx-slider", "bx-x")
+        popupMenu.style.display = 'flex'
+    }else {
+        toggleMenu.classList.replace("bx-x", "bx-slider")
         popupMenu.style.display = ''
     }   
 })
