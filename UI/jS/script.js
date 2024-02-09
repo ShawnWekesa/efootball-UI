4d0d78e0d4cada911db74328206d5a7afc5da3ae
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2024-02-09 14:35:37 +0300

    clear cache implemented

--- UI/jS/script.js
+++ UI/jS/script.js
@@ -12,3 +12,36 @@ toggleMenu.addEventListener('click', () => {
         popupMenu.style.display = ''
     }   
 })
+
+
+const clearCacheTrigger = document.querySelector('.clear-cache > span')
+const clearCacheDiv = document.querySelector('.cache-div');
+const clearCacheButtons = document.querySelectorAll('.cache-div div button')
+
+clearCacheTrigger.addEventListener("click", () => {
+    clearCacheDiv.style.display = "flex";
+})
+
+clearCacheButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault()
+    })
+})
+clearCacheButtons[0].addEventListener("click", () => {
+    clearCacheDiv.style.display = "none";
+})
+clearCacheButtons[1].addEventListener("click", () => {
+    const clearcacheSpan = document.querySelector('.cache-div span')
+    const clearcacheParagraph = document.querySelector('.cache-div p')
+
+    clearcacheSpan.innerText = "Cache Cleared"
+    clearcacheParagraph.innerText = "The process completed successfully"
+    clearCacheButtons[0].style.display = "none"
+    clearCacheButtons[1].style.display = "none"
+    clearCacheButtons[2].style.display = "block"
+
+    clearCacheButtons[2].addEventListener("click", () => {
+    clearCacheDiv.style.display = "none";
+})
+    console.log(clearcacheParagraph, clearcacheSpan)
+})
